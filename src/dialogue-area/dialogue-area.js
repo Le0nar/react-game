75207e5caa17b48144c12cf63e38874971134256
Yaroslav Trefilov <yaroslavtrefilov5@gmail.com>
2021-02-15 21:16:14 +0400

    add image partner

--- src/dialogue-area/dialogue-area.js
+++ src/dialogue-area/dialogue-area.js
@@ -2,14 +2,21 @@ import React from 'react';
 import './dialogue-area.scss';
 import DisplayText from '../display-text/display-text';
 import ChooseArea from '../choose-area/choose-area';
-
+import partnerImgSrc from '../images/hayako.png'
 
 class DialogueArea extends React.Component {
     render() {
+        // 
+        const partnerName = 'Hayako'
+        const partnerImg = <img src={partnerImgSrc} alt="Девочка"></img>
+        // 
         return (
         <div className="dialoque-area">
-            <div className="dialoque-area__name">
-                123
+            <div className="dialoque-area__top">
+                <div className="dialoque-area__name">
+                    {partnerName}
+                </div>
+                {partnerImg}
             </div>
             <div className="dialoque-area__display">
                 <ChooseArea />
