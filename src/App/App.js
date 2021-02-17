2ef8262a510fc553634f7aade44f1483c16ccaac
Yaroslav Trefilov <yaroslavtrefilov5@gmail.com>
2021-02-17 12:30:27 +0400

    add basic states structure for app

--- src/App/App.js
+++ src/App/App.js
@@ -2,15 +2,28 @@ import React from 'react';
 import './App.scss';
 
 import GameArea from '../game-area/game-area';
+import MainMenu from '../main-menu/main-menu';
 
 
 
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPage: MainMenu,
+      parnterName: 'Hayako',
+      music:'',
+      playerName: 'Player',
+      gameBackground: '',
+      gameTime:'',
+      gameScore:''
+    };
+  }
   render() {
     return (
       <div className="App">
-        <GameArea />
+        <this.state.currentPage />
       </div>
     );
   }
