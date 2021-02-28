d23f5d4c8d274625bd54f97ffdfed45ded47b3f7
Yaroslav Trefilov <yaroslavtrefilov5@gmail.com>
2021-03-01 00:03:11 +0400

    fix music

--- src/components/music/music.js
+++ src/components/music/music.js
@@ -2,8 +2,13 @@ import React from 'react';
 import soundSrc from '../../audio/Komiku-Road-2-Chill.mp3';
 
 class Music extends React.Component {
+  constructor(props) {
+    super(props);
+    this.musicRef = React.createRef();
+  }
   render() {
-    const audio = <audio autoPlay src={soundSrc} volume={0}/>;
+    const audio = <audio autoPlay src={soundSrc} ref={this.musicRef} volume={0.2} loop/>;
+
     return this.props.isMusicOn ? audio : false;
   }
 }
