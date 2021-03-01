ce30ab4fa85506a7f82377f751601ec479708c73
Yaroslav Trefilov <yaroslavtrefilov5@gmail.com>
2021-03-01 18:01:42 +0400

    add key event handler

--- src/components/enter-name/enter-name.js
+++ src/components/enter-name/enter-name.js
@@ -1,38 +1,55 @@
-import React from 'react';
-import './enter-name.scss';
+import React from "react";
+import "./enter-name.scss";
 
 class EnterName extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {playerName : 'Player'}
-    }
-    handleChange = (event) => {
-        this.setState({playerName: event.target.value})
-    }
-    changePlayerName = currentValue => {
-        if (currentValue === '') {
-            return 'Player';
-        } else {
-            return currentValue
-        }
-    }
-    startGame = () => {
-        this.props.onChangeState('pageName','GameArea');
-        const currentPlayerName = this.changePlayerName(this.state.playerName);
-        this.props.onChangeState('playerName', currentPlayerName);
+  constructor(props) {
+    super(props);
+    this.state = { playerName: "Player" };
+  }
 
-        this.props.setGameBackround()
+  handleChange = (event) => {
+    this.setState({ playerName: event.target.value });
+  }
+
+  changePlayerName = () => {
+    const currentName = this.state.playerName
+    if (currentName === "") {
+      return "Player";
+    } else {
+      return currentName;
     }
-    render() {
-        return(
-            <div className="enter-name">
-                <h3>Введите имя</h3>
-                <input autoFocus={true} onChange={this.handleChange} />
-                <button onClick={this.startGame}>OK</button>
-            </div>
-        )
+  }
+
+  startGameByPressing = (e) => {
+    if (e.key === "Enter") {
+        this.startGame();
     }
+  }
+
+  startGame = () => {
+    this.props.onChangeState("pageName", "GameArea");
+    const currentPlayerName = this.changePlayerName(this.state.playerName);
+    this.props.onChangeState("playerName", currentPlayerName);
+
+    this.props.setGameBackround();
+  }
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.startGameByPressing);
+  }
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.startGameByPressing);
+  }
+
+  render() {
+    return (
+      <div className="enter-name">
+        <h3>Введите имя</h3>
+        <input autoFocus={true} onChange={this.handleChange} />
+        <button onClick={this.startGame}>OK</button>
+      </div>
+    );
+  }
 }
-
 
 export default EnterName;
