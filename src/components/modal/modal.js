0d7b02939a6070d8bada179a30b8e67edf350ce4
Yaroslav Trefilov <yaroslavtrefilov5@gmail.com>
2021-03-01 21:31:01 +0400

    add finish btn for modal

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -3,21 +3,28 @@ import "./modal.scss";
 import AudioSettings from "../audio-settings/audio-settings";
 
 class Modal extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    closeModal = (e) => {
-        if (e.target.className === "close" || e.target.className === "modal") {
-            this.props.toggleModal();
-        }
+  constructor(props) {
+    super(props);
+  }
 
+  closeModal = (e) => {
+    if (e.target.className === "close" || e.target.className === "modal") {
+      this.props.toggleModal();
     }
+  };
+
+  goToEndGame = () => {
+    this.props.onChangeState("pageName", "EndGame");
+  };
+
   componentDidMount() {
     document.addEventListener("click", this.closeModal);
   }
+
   componentWillUnmount() {
     document.removeEventListener("click", this.closeModal);
   }
+
   render() {
     return (
       <div className="modal" id="myModal">
@@ -27,6 +34,9 @@ class Modal extends React.Component {
             <h2>Audio settings</h2>
           </div>
           <AudioSettings {...this.props} />
+          <button className="dialoque-area__btn" onClick={this.goToEndGame}>
+            Finish the game
+          </button>
         </div>
       </div>
     );
